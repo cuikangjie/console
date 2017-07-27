5fd2ff5a2ab157c0025f660c76052eb04aac5aa4
kin <cuikangjie_90h@126.com>
2017-07-27 17:27:03 +0800

    log

---
+++ app/app.js
@@ -0,0 +1,26 @@
+(function(window){
+  console.log();
+  console.log('------------------------------------------------------------------------------------------------------');
+  console.log('|                ==            ==         ==============           ||=              ||               |');
+  console.log('|                ==          ==                 ||                 || =             ||               |');
+  console.log('|                ==        ==                   ||                 ||  =            ||               |');
+  console.log('|                ==      ==                     ||                 ||   =           ||               |');
+  console.log('|                ==    ==                       ||                 ||    =          ||               |');
+  console.log('|                ==  ==                         ||                 ||     =         ||               |');
+  console.log('|                == =                           ||                 ||      =        ||               |');
+  console.log('|                == =                           ||                 ||       =       ||               |');
+  console.log('|                ==  ==                         ||                 ||        =      ||               |');
+  console.log('|                ==    ==                       ||                 ||         =     ||               |');
+  console.log('|                ==      ==                     ||                 ||          =    ||               |');
+  console.log('|                ==        ==                   ||                 ||           =   ||               |');
+  console.log('|                ==          ==                 ||                 ||            =  ||               |');
+  console.log('|                ==            ==               ||                 ||             = ||               |');
+  console.log('|                ==              ==       ==============           ||              =||               |');
+  console.log('------------------------------------------------------------------------------------------------------');
+  console.log('|                                                                                                    |');
+  console.log('|                                         前端工程师                                                  |');
+  console.log('|                                         home：cuikangjie.com                                       |');
+  console.log('|                                         mail: kin@cuikangjie.com                                   |');
+  console.log('|                                                                                                    |');
+  console.log();
+}))(window)
